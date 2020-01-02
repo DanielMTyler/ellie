5fadad50e64d58ff5141355bdbf441ad4c26992a
Daniel M Tyler <59429129+DanielMTyler@users.noreply.github.com>
2020-01-02 04:35:13 -0500

    Fixed if/else non-sense in run.bat

--- dev/win64/run.bat
+++ dev/win64/run.bat
@@ -9,26 +9,30 @@ rem
 setlocal EnableDelayedExpansion
 
 rem Use pushd to avoid changing the user's working directory.
-if exist ..\release (
-    pushd ..\release
+if exist ..\..\release (
+    pushd ..\..\release
 ) else (
-    echo ERROR: "..\release" directory doesn't exist.
+    echo ERROR: "..\..\release" directory doesn't exist.
     goto END
 )
 
 rem Release or Debug
 set BuildType=%1
+rem if /I doesn't allow for else statements.
+
 if /I %BuildType%=="release" (
+    set BuildType=0
     echo Running release build.
-    start /WAIT ..\bin\win64\ellie.exe
-) else (
-    if /I %BuildType%=="debug" (
-        echo Running debug build.
-        start /WAIT ..\bin\win64\ellie_debug.exe
-    ) else (
-        echo ERROR: Build Type must be supplied in first parameter and be equal to release or debug.
-        goto END
-    )
+    start /WAIT ..\..\bin\win64\ellie.exe
+)
+if /I %BuildType%=="debug" (
+    set BuildType=0
+    echo Running debug build.
+    start /WAIT ..\..\bin\win64\ellie_debug.exe
+)
+if %BuildType%==0 (
+    echo ERROR: Build Type must be supplied in first parameter and be equal to release or debug.
+    goto END
 )
 
 :END
